1fdca8ca9d9a4b261e9c646085dffc6b37cb4148
tangriane <tangrianekolisnichenco@gmail.com>
2017-12-14 00:57:31 -0200

    exerc 4 funcionando, fazer o 5

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -3,15 +3,33 @@ import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
-import {FormularioAutor, TabelaAutores} from './Autor'
+
 class App extends Component {
 
- 
+  constructor() {
+    super();    
+    this.state = {lista : [],nome:'',email:'',senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  componentDidMount(){  
+    $.ajax({
+        url:"https://cdc-react.herokuapp.com/api/autores",
+        dataType: 'json',
+        success:function(resposta){    
+          this.setState({lista:resposta});
+        }.bind(this)
+      } 
+    );          
+  }
 
   enviaForm(evento){
     evento.preventDefault();    
     $.ajax({
-      url:'http://localhost:8080/api/autores',
+      url:'https://cdc-react.herokuapp.com/api/autores',
       contentType:'application/json',
       dataType:'json',
       type:'post',
@@ -25,13 +43,23 @@ class App extends Component {
     });
   }
 
- 
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }  
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }
   render() {    
     return (
       <div id="layout">
-
+          
           <a href="#menu" id="menuLink" className="menu-link">
-
+              
               <span></span>
           </a>
 
@@ -44,7 +72,7 @@ class App extends Component {
                       <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
                       <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
 
-
+                      
                   </ul>
               </div>
           </div>
@@ -54,14 +82,44 @@ class App extends Component {
                     <h1>Cadastro de Autores</h1>
                   </div>
                   <div className="content" id="content">
-                  <FormularioAutor/>
-                  <TabelaAutores/>
-                    
-                           
+                    <div className="pure-form pure-form-aligned">
+                      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                        <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                        <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                        <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
+                        <div className="pure-control-group">                                  
+                          <label></label> 
+                          <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                        </div>
+                      </form>             
+
+                    </div>  
+                    <div>            
+                      <table className="pure-table">
+                        <thead>
+                          <tr>
+                            <th>Nome</th>
+                            <th>email</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {
+                            this.state.lista.map(function(autor){
+                              return (
+                                <tr key={autor.id}>
+                                  <td>{autor.nome}</td>
+                                  <td>{autor.email}</td>
+                                </tr>
+                              );
+                            })
+                          }
+                        </tbody>
+                      </table> 
+                    </div>             
                   </div>
                 </div>            
 
- 
+
       </div>     
     );
   }
